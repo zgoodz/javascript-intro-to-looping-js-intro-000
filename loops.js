b233894c47c801f07a307c00100d75f253a3b090
Zachary Goodman <zacharyrgoodman@gmail.com>
2021-05-02 18:28:46 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,5 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    console.log(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
+    return(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
   }
-  return forLoop(array)
 }
