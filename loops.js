b667eef3c54ba6610f3245ebf2264737001e6fae
Zachary Goodman <zacharyrgoodman@gmail.com>
2021-05-02 19:14:15 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,7 +15,12 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(num) {
-  
+  let i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
   
   do {
     console.log("I run once regardless.");
